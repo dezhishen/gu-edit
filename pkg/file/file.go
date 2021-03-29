dd1bcd28dfa9647d232d74ab5e577cb56a2a58ea
dezhiShen <1179551960@qq.com>
2021-03-29 10:44:26 +0800

    feat(file):完成文件读写

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -1,12 +1,21 @@
 package file
 
+import (
+	"io/ioutil"
+	"os"
+)
+
 // Read 读取
 func Read(path string) ([]byte, error) {
-
-	return nil, nil
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return ioutil.ReadAll(file)
 }
 
 // Save 保存
-func Save(path, content []byte) error {
-	return nil
+func Save(path string, content []byte) error {
+	return ioutil.WriteFile(path, content, 0644)
 }
