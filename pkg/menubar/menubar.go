42197fdb72a8ce4edc43a57a0849e66b89b499e3
dezhiShen <1179551960@qq.com>
2021-03-31 14:47:45 +0800

    feat(menubar):增加一些简单方法

--- pkg/menubar/menubar.go
+++ pkg/menubar/menubar.go
@@ -1,8 +1,43 @@
 package menubar
 
-import "github.com/therecipe/qt/widgets"
+import (
+	"sync"
+
+	"github.com/therecipe/qt/widgets"
+)
 
 type UIMenuBar struct {
-	Menubar *widgets.QMenuBar
-	Menus   []widgets.QMenu
+	menubar *widgets.QMenuBar
+	Menus   []UIMenu
+}
+
+type UIMenu struct {
+	menu    *widgets.QMenu
+	MenuBar *UIMenuBar
+	Active  bool
+	Index   int
+}
+
+var menuBarMenusLock sync.Mutex
+
+// 添加一个
+func (menuBar *UIMenuBar) AddMenu(menu UIMenu) error {
+	//加锁
+	menuBarMenusLock.Lock()
+	defer menuBarMenusLock.Unlock()
+	// dosomethings
+	menu.menu = widgets.NewQMenu(menuBar.menubar)
+	menu.MenuBar = menuBar
+	menu.Index = len(menuBar.Menus)
+	menuBar.Menus = append(menuBar.Menus, menu)
+	return nil
+}
+
+// 移除菜单
+func (menu *UIMenu) Remove() error {
+	//加锁
+	menuBarMenusLock.Lock()
+	defer menuBarMenusLock.Unlock()
+	menu.MenuBar.Menus = append(menu.MenuBar.Menus[:menu.Index], menu.MenuBar.Menus[menu.Index+1:]...)
+	return nil
 }
