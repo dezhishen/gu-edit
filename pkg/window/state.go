5acff9ae8a4b6448041632ef0f4945103474258b
dezhiShen <1179551960@qq.com>
2021-04-01 13:36:19 +0800

    feat(windom):初始化

--- pkg/window/state.go
+++ pkg/window/state.go
@@ -1,9 +1,21 @@
 package window
 
+import (
+	"os"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+)
+
 var Instance *UIMainWindow
 
 func Render() error {
 	// TODO 初始化
 	Instance = &UIMainWindow{}
+	widgets.NewQApplication(len(os.Args), os.Args)
+	qWindow := widgets.NewQMainWindow(nil, 0)
+	qWindow.SetObjectName("MainWindow")
+	qWindow.SetGeometry(core.NewQRect4(0, 0, 418, 483))
+	Instance.QWindow = qWindow
 	return nil
 }
