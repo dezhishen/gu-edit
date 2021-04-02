c113c5fab2e953efc190196d3c24f8486d64d93b
dezhiShen <1179551960@qq.com>
2021-04-02 14:10:26 +0800

    mod(textedit):增加文本编辑器

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,37 +1,29 @@
 package ui
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/dezhiShen/edit/internal/fileutil"
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
 )
 
-// 渲染页面
+// Render 渲染页面
 func Render() {
 	widgets.NewQApplication(len(os.Args), os.Args)
 	mainWindow := widgets.NewQMainWindow(nil, 0)
 	mainWindow.SetObjectName("MainWindow")
 	mainWindow.SetGeometry(core.NewQRect4(0, 0, 418, 483))
-	// centralwidget := widgets.NewQWidget(mainWindow, core.Qt__Widget)
-	// centralwidget.SetObjectName("Centralwidget")
 	gridLayout := widgets.NewQGridLayout(mainWindow)
 	gridLayout.SetObjectName("gridLayout")
 	gridLayout.SetContentsMargins(0, 0, 0, 0)
 	gridLayout.SetSpacing(0)
 	mainWindow.SetLayout(gridLayout)
-
 	tabWidget := widgets.NewQTabWidget(mainWindow)
 	tabWidget.SetObjectName("TabWidget")
-	Tab := widgets.NewQWidget(tabWidget, core.Qt__Widget)
-	Tab.SetObjectName("Tab")
-	tabWidget.AddTab(Tab, "tab1")
-	Tab2 := widgets.NewQWidget(tabWidget, core.Qt__Widget)
-	Tab2.SetObjectName("Tab2")
-	tabWidget.AddTab(Tab2, "tab2")
-	//gridLayout.AddWidget3(tabWidget,1, 0, 1, 1, 0)
 	mainWindow.SetCentralWidget(tabWidget)
-
 	menubar := widgets.NewQMenuBar(mainWindow)
 	menubar.SetObjectName("Menubar")
 	menubar.Show()
@@ -42,21 +34,20 @@ func Render() {
 	menuAction := menu.AddAction("打开")
 	menuAction.ConnectTriggered(func(checkBool bool) {
 		if !checkBool {
-			msg := widgets.NewQMessageBox(mainWindow)
-			msg.SetWindowTitle("yes comrade!")
-			msg.SetText("im opening now!")
-			okBtn := widgets.NewQPushButton(msg)
-			okBtn.SetText("ok")
-			msg.AddButton(okBtn,widgets.QMessageBox__AcceptRole)
-			msg.Exec()
-
+			path := widgets.QFileDialog_GetOpenFileName(mainWindow, "打开文本", "", "", "", widgets.QFileDialog__DontUseNativeDialog)
+			index := strings.LastIndex(path, "/")
+			fileName := path[index+1:]
+			content, _ := fileutil.Read(path)
+			textEdit := widgets.NewQTextEdit(tabWidget)
+			textEdit.SetObjectName(fileName)
+			textEdit.SetText(string(content))
+			tabWidget.AddTab(textEdit, fileName)
+			fmt.Println()
 		}
-		
+
 	})
 	menubar.AddMenu(menu)
 	mainWindow.SetMenuBar(menubar)
-	//gridLayout.SetMenuBar(menubar)
-	//gridLayout.AddWidget(tabWidget)
 	mainWindow.Show()
 	widgets.QApplication_Exec()
 }
